a2cb5035c8a20e583deab27db28a8cddc6edc256
jann <jannandrea.f@gmail.com>
2023-06-13 09:10:42 +0200

    wip: work milestones

--- Abschlussprojekt_ZLI/src/components/components/Experience.jsx
+++ Abschlussprojekt_ZLI/src/components/components/Experience.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
+import {motion} from 'framer-motion';
+import 'react-vertical-timeline-component/style.min.css';
+import { styles } from "../../styles";
+import { experiences } from "../../constants";
+import { SectionWrapper } from "../../hoc";
+import { textVariant } from "../../utils/motion";
+
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+    <motion.div>
+      <p className={styles.sectionSubText}>What actions have I taken up until now?</p>
+      <h2 className={styles.sectionHeadText}>Work Milestones.</h2>
+    </motion.div>
+
+<div className="mt-20 flex flex-col">
+  <VerticalTimeline>
+
+  </VerticalTimeline>
+</div>
+</>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work")
