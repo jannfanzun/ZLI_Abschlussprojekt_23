71be32575df9b19bfe71f11728fc7df0b85c872c
jann <jannandrea.f@gmail.com>
2023-06-13 08:59:39 +0200

    wip: padding on elements

--- Abschlussprojekt_ZLI/src/components/components/About.jsx
+++ Abschlussprojekt_ZLI/src/components/components/About.jsx
@@ -5,6 +5,7 @@ import {motion} from 'framer-motion';
 import { styles } from '../../styles';
 import { services } from '../../constants';
 import {fadeIn, textVariant} from '../../utils/motion'
+import { SectionWrapper } from '../../hoc';
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
@@ -20,7 +21,15 @@ const ServiceCard = ({ index, title, icon }) => (
         }}
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
-        
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
       </div>
     </motion.div>
   </Tilt>
@@ -50,4 +59,6 @@ const About = () => {
   );
 };
 
-export default About
+export default SectionWrapper(About, "about")
+
+// hilft uns den Seitenabstand zu halten
