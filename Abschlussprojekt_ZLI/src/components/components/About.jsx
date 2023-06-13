6b0f2de585dfeb29ee9781404b907af2b323ff95
jann <jannandrea.f@gmail.com>
2023-06-13 08:23:51 +0200

    wip: overview

--- Abschlussprojekt_ZLI/src/components/components/About.jsx
+++ Abschlussprojekt_ZLI/src/components/components/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import Tilt from "react-parallax-tilt"
+import {motion} from 'framer-motion';
+
+import { styles } from '../../styles';
+import { services } from '../../constants';
+import {fadeIn, textVariant} from '../../utils/motion'
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur rem corporis officiis animi dignissimos eveniet nisi odit, est tempora fugit modi repellat unde inventore sunt natus a explicabo error enim labore! Saepe molestias, vel dolorum, error ipsum, sit modi inventore voluptatem distinctio corporis recusandae earum?
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default About
