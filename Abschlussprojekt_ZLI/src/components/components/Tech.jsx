1ae5b1ff67c797a2b572520c36020ae44d732159
jann <jannandrea.f@gmail.com>
2023-06-13 10:07:54 +0200

    wip: skills balls

--- Abschlussprojekt_ZLI/src/components/components/Tech.jsx
+++ Abschlussprojekt_ZLI/src/components/components/Tech.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { SectionWrapper } from '../../hoc'
+import { technologies } from '../../constants'
+import { BallCanvas } from './canvas'
+import Ball from './canvas/Ball'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-12'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
